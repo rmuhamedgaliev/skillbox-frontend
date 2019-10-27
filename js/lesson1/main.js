b191fbdacec12db3bb6347d98ab489e5958fbd03
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-10-28 00:18:54 +0300

    homework1

---
+++ js/lesson1/main.js
@@ -0,0 +1,14 @@
+var myName = prompt('Как вас зовут?');
+var mySecondName = prompt('Введите вашу фамилию')
+var myBirthYear = prompt('Ведите ваш год рождения');
+
+var currentYear = 2019;
+var age = currentYear - myBirthYear;
+
+if (age < 20) {
+    alert('Привет ' + myName + ' ' + mySecondName + '!');
+} else if (age >= 20 && age < 40) {
+    alert('Добрый день, ' +  myName + ' ' + mySecondName);
+} else {
+    alert('Здравствуйте, ' +  myName + ' ' + mySecondName);
+}
