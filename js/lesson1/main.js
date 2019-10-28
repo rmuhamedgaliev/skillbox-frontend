9a3942e09581ab83bc7c106a1e0f01642816e7ab
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2019-10-29 00:32:29 +0300

    fixed date selection

--- js/lesson1/main.js
+++ js/lesson1/main.js
@@ -1,9 +1,9 @@
-var myName = prompt('Как вас зовут?');
-var mySecondName = prompt('Введите вашу фамилию');
-var myBirthYear = prompt('Ведите ваш год рождения');
+const myName = prompt('Как вас зовут?');
+const mySecondName = prompt('Введите вашу фамилию');
+const myBirthYear = prompt('Ведите ваш год рождения');
 
-var currentYear = new Date().getFullYear();
-var age = currentYear - myBirthYear;
+const currentYear = new Date().getFullYear();
+const age = currentYear - myBirthYear;
 
 if (age < 20) {
     alert('Привет ' + myName + ' ' + mySecondName + '!');
